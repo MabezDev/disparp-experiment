eed5e24e4c6083e072160bfd4687a22b0eff2ec8
Scott Mabin <MabezDev@gmail.com>
2018-12-27 18:02:22 +0000

    Back to rocket, using dumpcap from wireshark now - all using local loop back for experimenting, filtering port 8000 to only use record the basestation traffic

---
+++ README.md
@@ -0,0 +1,18 @@
+# DISPARP
+
+## Setup
+
+- The following ports must be available 
+    ```
+    8000 --> Base station
+    8001 --> Cluser 1
+    8002 --> Cluser 2
+    8003 --> Cluser 3
+    ```
+- run `cargo -p base_station` to launch the base station. This can be left running for the entirety of the experiments
+
+
+## The experiment scripts
+
+- `record` script records the exchnages around the basestation
+
--- base_station/src/main.rs
+++ base_station/src/main.rs
@@ -1,80 +1,13 @@
-#[macro_use]
-extern crate log;
-extern crate env_logger;
-extern crate getopts;
-extern crate smoltcp;
+#![feature(proc_macro_hygiene, decl_macro)]
 
-mod utils;
+#[macro_use] extern crate rocket;
 
-use std::str;
-use std::collections::BTreeMap;
-use std::fmt::Write;
-use std::os::unix::io::AsRawFd;
-use smoltcp::phy::wait as phy_wait;
-use smoltcp::wire::{EthernetAddress, IpAddress, IpCidr};
-use smoltcp::iface::{NeighborCache, EthernetInterfaceBuilder};
-use smoltcp::socket::SocketSet;
-use smoltcp::socket::{UdpSocket, UdpSocketBuffer, UdpPacketMetadata};
-use smoltcp::socket::{TcpSocket, TcpSocketBuffer};
-use smoltcp::time::{Duration, Instant};
+#[post("/node", data = "<input>")]
+fn node(input: String) -> String {
+    let _x = input; // we dont do anything with the input, this sqwelches the warning
+    "{ status: \"ok\" }".to_string()
+}
 
 fn main() {
-    // utils::setup_logging("");
-
-    let (mut opts, mut free) = utils::create_options();
-    utils::add_tap_options(&mut opts, &mut free);
-    utils::add_middleware_options(&mut opts, &mut free);
-
-    let mut matches = utils::parse_options(&opts, free);
-    let device = utils::parse_tap_options(&mut matches);
-    let fd = device.as_raw_fd();
-    let device = utils::parse_middleware_options(&mut matches, device, /*loopback=*/false);
-
-    let neighbor_cache = NeighborCache::new(BTreeMap::new());
-
-    let tcp1_rx_buffer = TcpSocketBuffer::new(vec![0; 1024]);
-    let tcp1_tx_buffer = TcpSocketBuffer::new(vec![0; 1024]);
-    let tcp1_socket = TcpSocket::new(tcp1_rx_buffer, tcp1_tx_buffer);
-
-    let ethernet_addr = EthernetAddress([0x02, 0x00, 0x00, 0x00, 0x00, 0x01]);
-    let ip_addrs = [
-        IpCidr::new(IpAddress::v4(192, 168, 69, 100), 24)
-    ];
-    let mut iface = EthernetInterfaceBuilder::new(device)
-            .ethernet_addr(ethernet_addr)
-            .neighbor_cache(neighbor_cache)
-            .ip_addrs(ip_addrs)
-            .finalize();
-
-    let mut sockets = SocketSet::new(vec![]);
-    let tcp1_handle = sockets.add(tcp1_socket);
-
-    loop {
-        let timestamp = Instant::now();
-        match iface.poll(&mut sockets, timestamp) {
-            Ok(_) => {},
-            Err(e) => {
-                debug!("poll error: {}", e);
-            }
-        }
-
-    
-
-        // tcp:8000: respond "hello"
-        {
-            let mut socket = sockets.get::<TcpSocket>(tcp1_handle);
-            if !socket.is_open() {
-                socket.listen(8000).unwrap();
-            }
-
-            // if socket.can_send() {
-            //     debug!("tcp:8000 send greeting");
-            //     write!(socket, "{{ status: \"ok\" }}").unwrap();
-            //     debug!("tcp:8000 close");
-            //     socket.close();
-            // }
-        }
-
-        phy_wait(fd, iface.poll_delay(&sockets, timestamp)).expect("wait error");
-    }
+    rocket::ignite().mount("/", routes![node]).launch();
 }
