ee2b9aefac7a78ce230fa1d74db1be9905631359
Scott Mabin <MabezDev@gmail.com>
2018-12-27 13:23:31 +0000

    Basic node done, need to start posting

--- base_station/src/main.rs
+++ base_station/src/main.rs
@@ -2,11 +2,6 @@
 
 #[macro_use] extern crate rocket;
 
-// #[get("/hello/<name>/<age>")]
-// fn hello(name: String, age: u8) -> String {
-//     format!("Hello, {} year old named {}!", age, name)
-// }
-
 #[post("/node", data = "<input>")]
 fn node(input: String) -> String {
     let _x = input; // we dont do anything with the input, this sqwelches the warning
--- node/src/main.rs
+++ node/src/main.rs
@@ -3,19 +3,65 @@
 extern crate reqwest;
 extern crate env_logger;
 
+
+use rand::prelude::*;
+
+// #[macro_use] extern crate structopt;
+
+use structopt::StructOpt;
+use std::{thread, time};
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "node")]
+struct Opt {
+    /// Enable more output
+    #[structopt(short = "d", long = "debug")]
+    debug: bool,
+    /// Post location
+    #[structopt(short = "o", long = "output")]
+    output: String,
+    /// Repeat the transmition
+    #[structopt(short = "r", long = "repeat")]
+    repeat: bool,
+    /// Interval between repeat transmitions in ms - 0 will choose a random repeat value between 0 and 1000ms
+    #[structopt(short = "i", long = "interval", default_value = "1000")]
+    interval: u64
+}
+
 fn main() -> Result<(), Box<std::error::Error>> {
     env_logger::init();
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+    let mut rng = rand::thread_rng();
 
-    println!("GET https://www.rust-lang.org");
+    if opt.repeat {
+        loop {
+            run_req(&opt)?;
+            let interval = if opt.interval == 0 {
+                rng.gen_range(0u64, 1000u64)
+            } else {
+                opt.interval
+            };
+            thread::sleep(time::Duration::from_millis(interval));
+        }
+    } else {
+        run_req(&opt)?;
+    }
 
-    let mut res = reqwest::get("https://www.rust-lang.org/en-US/")?;
-
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:?}", res.headers());
-
-    // copy the response body directly to stdout
-    std::io::copy(&mut res, &mut std::io::stdout())?;
+    
 
     println!("\n\nDone.");
+    Ok(())
+}
+
+fn run_req(opt: &Opt) -> Result<(), Box<std::error::Error>> {
+    let mut res = reqwest::get(opt.output.as_str())?;
+    println!("Status: {}", res.status());
+
+    // copy the response body directly to stdout
+    if opt.debug {
+        println!("Headers:\n{:?}", res.headers());
+        std::io::copy(&mut res, &mut std::io::stdout())?;
+    }
     Ok(())
 }
