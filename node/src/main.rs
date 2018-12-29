088120ea7a7395c9ae984c2365108fb30d331a63
Scott Mabin <MabezDev@gmail.com>
2018-12-29 15:59:21 +0000

    rayon experiments

--- node/src/main.rs
+++ node/src/main.rs
@@ -6,7 +6,7 @@ extern crate env_logger;
 
 use rand::prelude::*;
 
-// #[macro_use] extern crate structopt;
+use rayon::prelude::*;
 
 use structopt::StructOpt;
 use std::{thread, time};
@@ -25,29 +25,36 @@ struct Opt {
     repeat: bool,
     /// Interval between repeat transmitions in ms - 0 will choose a random repeat value between 0 and 1000ms
     #[structopt(short = "i", long = "interval", default_value = "1000")]
-    interval: u64
+    interval: u64,
+    /// Number of 'nodes' to simulate
+    #[structopt(short = "c", long = "count", default_value = "100")]
+    nodes: u32
 }
 
 fn main() -> Result<(), Box<std::error::Error>> {
     env_logger::init();
     let opt = Opt::from_args();
     println!("{:?}", opt);
-    let mut rng = rand::thread_rng();
-    let mut client = reqwest::Client::new();
-
-    if opt.repeat {
-        loop {
-            run_req(&mut client, &opt)?;
-            let interval = if opt.interval == 0 {
-                rng.gen_range(0u64, 1000u64)
-            } else {
-                opt.interval
-            };
-            thread::sleep(time::Duration::from_millis(interval));
+    
+    let _ = (0..opt.nodes).into_par_iter().map(|i| {
+        let mut rng = rand::thread_rng();
+        let mut client = reqwest::Client::new();
+        println!("Starting node {}", i);
+        if opt.repeat {
+            loop {
+                run_req(&mut client, &opt).unwrap();
+                let interval = if opt.interval == 0 {
+                    rng.gen_range(0u64, 1000u64)
+                } else {
+                    opt.interval
+                };
+                thread::sleep(time::Duration::from_millis(interval));
+            }
+        } else {
+            run_req(&mut client, &opt).unwrap();
         }
-    } else {
-        run_req(&mut client, &opt)?;
-    }
+    });
+    
 
     
 
