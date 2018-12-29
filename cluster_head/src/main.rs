4893c4e42952c8b9ed1c1dd0a91a7fead2bbe15d
Scott Mabin <MabezDev@gmail.com>
2018-12-29 17:59:06 +0000

    Added the code for the cluser head, setup bash to run the experiments, cna now run experiment 1 and 2. Final experiment needs aggreagtion implemented on the cluster head

--- cluster_head/src/main.rs
+++ cluster_head/src/main.rs
@@ -1,3 +1,34 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+use structopt::StructOpt;
+
+#[macro_use] extern crate rocket;
+
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(name = "node")]
+struct Opt {
+    /// Enable more output
+    #[structopt(short = "d", long = "debug")]
+    debug: bool,
+    /// Post location
+    #[structopt(short = "o", long = "output")]
+    output: String,
+    /// Number of payloads to aggregate in memory before sending - 0 doesnt not aggregate
+    #[structopt(short = "c", long = "count", default_value = "100")]
+    payload_count: u32
+}
+
+#[post("/node", data = "<input>")]
+fn node(input: String) -> String {
+    let opt = Opt::from_args();
+    let client = reqwest::Client::new();
+    let mut vec = vec![0u8; input.len()];
+    vec.clone_from_slice(&input.as_bytes());
+    let _ = client.post(opt.output.as_str())
+        .body(vec)
+        .send().unwrap();
+    "{ status: \"ok\" }".to_string()
+}
+
 fn main() {
-    println!("Hello, world! Cluster");
+    rocket::ignite().mount("/", routes![node]).launch();
 }
