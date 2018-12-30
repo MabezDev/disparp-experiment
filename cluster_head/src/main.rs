e6b115144d20f649747e460d09663490a5a3b00b
Scott Mabin <MabezDev@gmail.com>
2018-12-30 17:09:15 +0000

    Sleep to let servers boot. Actually aggregate properly

--- cluster_head/src/main.rs
+++ cluster_head/src/main.rs
@@ -22,19 +22,28 @@ struct Opt {
 
 
 #[post("/node", data = "<input>")]
-fn node(input: String, opt: State<Opt>, payloads: State<Mutex<Vec<Vec<u8>>>>) -> String {
+fn node(input: String, opt: State<Opt>, payloads: State<Mutex<Vec<Vec<u8>>>>, byte_count: State<Mutex<usize>>) -> String {
     let client = reqwest::Client::new();
     let mut vec = vec![0u8; input.len()];
     vec.clone_from_slice(&input.as_bytes());
     if opt.payload_count > 0 {
+        let mut bytes = byte_count.lock().unwrap();
+        *bytes += input.len();
         let mut payloads = payloads.lock().unwrap();
         payloads.push(vec);
         if payloads.len() > opt.payload_count {
-            for vec in payloads.clone().into_iter() {
-                let _ = client.post(opt.output.as_str())
-                    .body(vec)
-                    .send().unwrap();
+            let mut buffer = Vec::with_capacity(*bytes);
+            for mut vec in payloads.clone().into_iter() {
+                buffer.append(&mut vec);
             }
+            let _ = client.post(opt.output.as_str())
+                    .body(buffer) // send buffer in 1 go
+                    .send().unwrap();
+            // for vec in payloads.clone().into_iter() {
+            //     let _ = client.post(opt.output.as_str())
+            //         .body(vec)
+            //         .send().unwrap();
+            // }
             payloads.clear();
         }
     } else {
@@ -48,8 +57,10 @@ fn node(input: String, opt: State<Opt>, payloads: State<Mutex<Vec<Vec<u8>>>>) ->
 fn main() {
     let opt = Opt::from_args();
     let payloads: Mutex<Vec<Vec<u8>>> = Mutex::new(vec![]);
+    let byte_count: Mutex<usize> = Mutex::new(0);
     rocket::ignite()
         .manage(opt)
         .manage(payloads)
+        .manage(byte_count)
         .mount("/", routes![node]).launch();
 }
